78927333113871eae17a496a870a2b8d6dec573e
Junior Pessoa <ejunior.pessoa@gmail.com>
2022-02-10 20:43:39 -0300

    SpaceBlog

---
+++ script/menu-mobile.js
@@ -0,0 +1,5 @@
+// const menu = document.querySelector(".container-menu img");
+// const activeClass = "menu-mobile";
+// showMenu = (event) => { event.currentTarget.classList.toggle(activeClass); }
+// menu.addEventListener("click", showMenu);
+
